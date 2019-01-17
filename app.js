db16b5e40e8483ea86a07b2d73044431a28acee9
Deb Ray <debabrata.samantaray@gmail.com>
2019-01-17 22:29:45 +0800

    testing openshift

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello - from Webhook!!!'))
+app.get('/', (req, res) => res.send('Hello - from deb !!!'))
 
 app.get('/anotherhello', (req, res) => res.send('Another Hello!!'))
 
